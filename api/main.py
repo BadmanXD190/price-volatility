6879a7dc912938a34a107cd56fc1ca12685ed5f1
BadmanXD190 <83124886+BadmanXD190@users.noreply.github.com>
2025-10-07 19:52:34 +0800

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,12 +1,11 @@
 from fastapi import FastAPI
 
-# Tell FastAPI it's mounted under /api
-app = FastAPI(root_path="/api")
+app = FastAPI()
 
-@app.get("/")
+@app.get("/api")
 async def read_root():
     return {"message": "Hello, Vercel!"}
 
-@app.get("/hello")
+@app.get("/api/hello")
 async def read_hello():
     return {"message": "Hello endpoint from FastAPI"}
